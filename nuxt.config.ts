5817fe971fd02750dc82039b6a33bc0320a8d358
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-03 17:08:40 +0530

    ntl-9: Fixed build issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
-  eslint: {
-    config: {
-      standalone: false,
-    },
-  },
+  // register eslint module with options
+  modules: [["@nuxt/eslint", { config: { standalone: false } }]],
 });
