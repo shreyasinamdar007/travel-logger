2e371c43c0b703453be846390fad74f0e58f55da
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-04 09:59:43 +0530

    ntl-10: Setup Taiwind / DaisyUI

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      Button
+      <Icon name="tabler:map" class="ml-2" />
+    </button>
   </div>
 </template>
