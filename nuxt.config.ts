f02950d4a6c8e828705375fa7d31d3bc166c3032
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-06 18:32:45 +0530

    ntl-11: #11 Fixed some minor configuration issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   // register eslint module with options
-  modules: [["@nuxt/eslint", { config: { standalone: false } }]],
+  modules: [["@nuxt/eslint", { config: { standalone: false } }], "@nuxt/icon"],
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: ["@nuxt/devtools", "@vue/devtools-core", "@vue/devtools-kit"],
+    },
+  },
 });
