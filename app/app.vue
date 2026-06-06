245f3cae814babe0bf3ef31006510293b0f5825e
shreyasinamdar007 <35952498+shreyasinamdar007@users.noreply.github.com>
2026-06-06 18:20:00 +0530

    ntl-11: #11 Added Hero Section and Login button UI (#3)

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      Button
-      <Icon name="tabler:map" class="ml-2" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
