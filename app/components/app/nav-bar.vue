84ac0050277c6c3d3ca1d782315b4a71b1de56b9
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-06 18:17:31 +0530

    ntl-11: #11 Added Hero Section and Login button UI

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar relative bg-primary text-primary-content px-1">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-md">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <NuxtLink to="/signin" class="btn btn-primary text-black text-md bg-accent hover:bg-teal-500 hover:text-white">
+        Sign In with GitHub
+        <Icon name="tabler:brand-github" size="24" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and share your experiences with friends and family. Create a travel log to document
+          your adventures and inspire others to explore the world.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
