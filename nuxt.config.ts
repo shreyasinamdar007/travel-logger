d957e56a53dcaa7079dbff6a5575b70d4cc1f1c0
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-06 19:35:01 +0530

    ntl-13: Added env configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
+
+import "./lib/env";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
