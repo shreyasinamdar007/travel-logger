7b0a94cb08226c6c88dee7ede451762783e35cf8
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2026-06-07 16:36:59 +0530

    ntl-14: add DB schemas and configured SQLlite

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+import env from "./lib/env";
+
+export default defineConfig({
+  out: "./lib/db/migrations",
+  schema: "./lib/db/schema/index.ts",
+  dialect: "turso",
+  casing: "snake_case",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL!,
+    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN!,
+  },
+});
