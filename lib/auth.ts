e8cbf5726c0aa43ad49b81d4ab5c33e08c3cec1b
shreyasinamdar007 <35952498+shreyasinamdar007@users.noreply.github.com>
2026-06-10 19:35:23 +0530

    Ntl 16 (#11)
    * ntl-16: Added better auth and Github authentication
    
    * ntl-16: Added Error handling
    
    * ntl-16: Added Error handling
    
    * ntl-16: Fixed issues
    
    * ntl-16: Fixed issues
    
    * ntl-16: Fixed issues

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,12 +2,14 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index";
+import * as schema from "./db/schema";
 import env from "./env";
 
 export const auth = betterAuth({
   secret: env.BETTER_AUTH_TOKEN,
   database: drizzleAdapter(db, {
     provider: "sqlite",
+    schema,
   }),
   advanced: {
     database: {
